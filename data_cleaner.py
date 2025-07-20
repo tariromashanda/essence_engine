55ae6b6c8d2a9ceeeccf175dcfd187c1b219217b
tamashajhb024 <tamashajhb024@student.wethinkcode.co.za>
2025-07-20 08:46:57 +0200

    data_cleaner:remove unnecessary coloumns

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,9 +1,32 @@
 from csv import DictReader
+import csv
 
-with open('fra_perfumes.csv', 'r') as file:
-    dict_reader = DictReader(file)
+header = ["Name", "Gender","Main Accords", "Description"]
+
+output_file = 'clean_data.csv'
+input_file = 'fra_perfumes.csv'
+
+with open(output_file, 'a', newline='') as outfile:
+    with open(input_file, 'r') as infile:
+
+        writer = csv.writer(outfile)
+        if outfile.tell()== 0:
+            writer.writerow(header)
+        reader = csv.reader(infile)
+        header = next(reader)
+
+        for row in reader:
+          new_row = [row[0], row[1], row[4], row[6]]
+          writer.writerow(new_row)
+        
+
+print(f"CSV file {output_file} created")
+
+
+# with open('fra_perfumes.csv', 'r') as file:
+#     dict_reader = DictReader(file)
     
-    list_of_dict = list(dict_reader)
+#     list_of_dicts = list(dict_reader)
 
-print(list_of_dict[1])
+# print(list_of_dicts[0])
     
