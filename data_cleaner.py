f64a7936c9a75be7f4fe958096df8d6208a741e1
tamashajhb024 <tamashajhb024@student.wethinkcode.co.za>
2025-07-20 09:30:40 +0200

    data_cleaner: modified chnage_gender function

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,6 +1,15 @@
 from csv import DictReader
 import csv
 
+def change_gender(gender):
+    gender = gender.split(" ")
+    if 'women' in gender and 'men' in gender:
+        return "unisex"
+    elif 'women' in gender:
+        return 'female'
+    elif "men" in gender:
+        return 'male'
+            
 header = ["Name", "Gender","Main Accords", "Description"]
 
 output_file = 'clean_data.csv'
@@ -16,13 +25,16 @@ with open(output_file, 'a', newline='') as outfile:
         header = next(reader)
 
         for row in reader:
-          if row[4] == '[]':
-              continue
-          new_row = [row[0], row[1], row[4], row[6]]
-          writer.writerow(new_row)
+            if row[4] == '[]':
+               continue
+            row_one = change_gender(row[1])
+            new_row = [row[0], row_one, row[4], row[6]]
+            writer.writerow(new_row)
         
 
 print(f"CSV file {output_file} created")
+
+
 
 
 # with open('fra_perfumes.csv', 'r') as file:
