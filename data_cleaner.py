b6f3e5a590574e38809b4c6cb2b1085ea452e875
tamashajhb024 <tamashajhb024@student.wethinkcode.co.za>
2025-07-20 11:38:47 +0200

    data_cleaner: create change_notes

--- data_cleaner.py
+++ data_cleaner.py
@@ -29,11 +29,42 @@ def change_name(name):
         new_name = name = re.sub(pattern_three, "", name)
     return new_name
 
-header = ["Name", "Gender","Main Accords", "Description"]
+def change_notes(notes):
+    notes = notes.lower()
+    pattern_top_notes = r"top note.*?;"
+    pattern_middle_notes = r"middle note.*?;"
+    pattern_base_notes = r"base note.*?[?.!]$"
+
+    match_top_notes = re.search(pattern_top_notes, notes)
+    match_middle_notes = re.search(pattern_middle_notes, notes)
+    match_base_notes = re.search(pattern_base_notes, notes)
+
+    notes = []
+
+    if match_top_notes:
+        top_notes = match_top_notes.group()
+        notes.append(top_notes)
+    else:
+        notes.append(0)
+
+    if match_middle_notes:
+        middle_notes = match_middle_notes.group()
+        notes.append(middle_notes)
+    else:
+        notes.append(0)
+
+    if match_base_notes:
+        base_notes = match_base_notes.group()
+        notes.append(base_notes)
+    else:
+        notes.append(0)
+    
+    return notes
+
+header = ["Name", "Gender","Olfactory Family", "Description"]
 
 output_file = 'clean_data.csv'
 input_file = 'fra_perfumes.csv'
-
 
 with open(output_file, 'a', newline='') as outfile:
     with open(input_file, 'r') as infile:
@@ -47,9 +78,13 @@ with open(output_file, 'a', newline='') as outfile:
         for row in reader:
             if row[4] == '[]':
                continue
-            row_one = change_gender(row[1])
             row_zero = change_name(row[0])
-            new_row = [row_zero, row_one, row[4], row[6]]
+            row_one = change_gender(row[1])
+            row_four = change_notes(row[6])
+
+            if 0 in row_four:
+                continue
+            new_row = [row_zero, row_one, row[4], row_four]
             writer.writerow(new_row)
         
 
