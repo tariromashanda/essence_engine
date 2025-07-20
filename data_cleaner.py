4abd171d1550057ab7c9a78ceae5e79dbc0693af
tamashajhb024 <tamashajhb024@student.wethinkcode.co.za>
2025-07-20 07:45:03 +0200

    data_cleaner: read csv file as a list of dicts

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,1 +1,9 @@
-import csv
+from csv import DictReader
+
+with open('fra_perfumes.csv', 'r') as file:
+    dict_reader = DictReader(file)
+    
+    list_of_dict = list(dict_reader)
+
+print(list_of_dict[1])
+    
